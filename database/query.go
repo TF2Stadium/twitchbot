c5a08c8316d79bbadb016bb849768dedae457706
Vibhav Pant <vibhavp@gmail.com>
2016-03-16 02:21:47 +0530

    Use correct query, return lobbyURL

--- database/query.go
+++ database/query.go
@@ -11,12 +11,12 @@ import (
 func GetCurrentLobby(channel string) string {
 	var playerid, lobbyid uint
 	db.QueryRow("SELECT id FROM players WHERE twitch_name = $1", channel).Scan(&playerid)
-	err := db.QueryRow("SELECT lobby_slots.lobby_id FROM lobbies INNER JOIN lobby_slots ON lobbies.id = lobby_slots.lobby_id WHERE lobbies.state = $1", models.LobbyStateWaiting).Scan(&lobbyid)
+	err := db.QueryRow("SELECT lobby_slots.lobby_id FROM lobbies INNER JOIN lobby_slots ON lobbies.id = lobby_slots.lobby_id WHERE lobbies.state = $1 AND lobby_slots.player_id = $2", models.LobbyStateWaiting, playerid).Scan(&lobbyid)
 	if err != nil {
 		return ""
 	}
 
 	lobbyURL, _ := url.Parse(config.Constants.FrontendURL)
 	lobbyURL.Path = fmt.Sprintf("lobby/%d", lobbyid)
-	return fmt.Sprintf(lobbyURL.String())
+	return lobbyURL.String()
 }
